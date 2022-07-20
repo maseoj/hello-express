3e8eb8849adff229f1cc4614f062ceda66c15f73
maseoj <109613619+maseoj@users.noreply.github.com>
2022-07-20 14:54:11 +0800

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const PORT = 3000;
+
+const app = express();
+
+function logger(req, res, next){
+    console.log(`[${Date.now()}] ${req.method} ${req.url}`);
+    next();
+}
+
+app.use(logger);
+
+app.get('/',(req,res) => {
+    res.json({ ok: true});
+});
+
+app.get('//:name',(req, res) => {
+    res.json({ greeting: `Hello ${req.params.name}!` });
+});
+
+app.listen(PORT, () => console.log ('Server is now listening on port ${PORT}'));
